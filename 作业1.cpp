36326a48d2e509166b9992542492dc63c19f43c1
EricXu1 <36099609+EricXu1@users.noreply.github.com>
2018-09-11 20:27:54 +0800

    end
    9.11

--- 作业1.cpp
+++ 作业1.cpp
@@ -1,11 +1,18 @@
-void del(SqList &L, ElemType a, ElemType b)
+#include <iostream>
+void delete(SqList &L, ElemType a, ElemType b)
 {
-	int i = 0, k = 1;
+	int i=0, k=1;
 	while (i<L.length)
 	{
-		if (L.elem[i] >= a && L.elem[i] < +b)k++;
-		else    L.elem[i - k] = L.elem[i];
+		if (L.elem[i]>=a&&L.elem[i]<= b)
+		{
+			k++;
+		}
+		else
+		{
+			L.elem[i-k] = L.elem[i];
+		}
 		i++;
 	}
-	L.length = L.length - k;
+	L.length = L.length -k;
 }
--- 作业2.cpp
+++ 作业2.cpp
@@ -1,9 +1,13 @@
-DataType min_seq(PSeqList palist) {    /*求非空顺序表中的最小数据元素*/
+#include<iostream>
+DataType min_seq(PSeqList palist) 
+{
 	DataType min;
-	inti;
-	min = palist - ＞element[0];    /*初始化min*/
-	for (i = 1; i＜palist - ＞n; i++)    /*min中保存的总是当前的最小数据*/
+	int i;
+	min = palist - ＞element[0];
+	for (i = 1; i＜palist - ＞n; i++)
 		if (min＞palist - ＞element[i])
+		{
 			min = palist - ＞elemellt[i];
+		}
 	return min;
 }
