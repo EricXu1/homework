df287b8190529a60c9fdec94c3a6845f75c44e18
EricXu1 <36099609+EricXu1@users.noreply.github.com>
2018-09-11 22:33:57 +0800

    en
    9.11.4

--- 作业1.cpp
+++ 作业1.cpp
@@ -1,7 +1,7 @@
 //默认已定义好顺序表L，elem是已经定义好的顺序结构，length为定义过的长度整型，头文件省略。
 void delete(SqList &L, ElemType a, ElemType b)
 {
-	int i=0, k=1;
+	int i=0, k=0;
 	while (i<L.length)
 	{
 		if (L.elem[i]>=a&&L.elem[i]<= b)
